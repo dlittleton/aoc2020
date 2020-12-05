bcb626fbd9074fabf57f1939f651fcf072146673
Duane Littleton <dlittleton87@gmail.com>
2020-12-05 15:20:48 -0500

    Convert as binary string instead of partitioning

--- 5/5-1.py
+++ 5/5-1.py
@@ -1,29 +1,13 @@
-import math
 import sys
 
 
-def partition(lo, hi, seq):
-    ''' seq is a sequence of booleans 
-    
-    True -- lower half
-    False -- upper half
-    '''
-    for x in seq:
-        half = math.ceil((hi - lo) / 2)
-        if x:
-            hi = lo + half
-        else:
-            lo = lo + half
-
-    return lo
-
-
 def position(line):
-    rows = line[0:7]
-    cols = line[7:10]
+    rows = line[0:7].replace('F', '0').replace('B', '1')
+    cols = line[7:10].replace('L', '0').replace('R', '1')
 
-    r = partition(0, 127, (c == 'F' for c in rows))
-    c = partition(0, 7, (c == 'L' for c in cols))
+    r = int(rows, 2)
+    c = int(cols, 2)
+
     return r * 8 + c
 
 
